9d13429e8179c1eddd3ad015127c87dcefc066ef
Juan D Chaves <juandavidchaves@protonmail.com>
2021-09-22 15:03:21 -0300

    File structure added

---
+++ juegos/juego1/juego1.js
@@ -0,0 +1,6 @@
+const inicio = document.querySelector(".iniciar")
+const espacioParaPregunta = document.querySelector(".espacio-para-pregunta")
+const respuesta = document.querySelector(".enviar-respuesta")
+const comodin = document.
+
+respuesta.addEventListener("click", (e) => console.log("arranque"))
