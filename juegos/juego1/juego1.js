e5a4deb03e3617ce555273281e3e3fb0c2022b09
Juan D Chaves <juandavidchaves@protonmail.com>
2021-09-23 23:47:53 -0300

    Added most of first game functionality

--- juegos/juego1/juego1.js
+++ juegos/juego1/juego1.js
@@ -1,21 +1,48 @@
 // En las siguientes líneas declaro variables: inicio, espacioParaPregunta, enviarRespuesta y comodin XXXXXX que corresponden a botones en la página y al campo en donde van las preguntas, las respuestas y el resultado final. 
 
 const inicio = document.querySelector(".iniciar")
-const comodin = document.getElementById("comodin")
+// const comodin = document.getElementById("comodin")
 const espacioParaPregunta = document.getElementById("espacio-para-pregunta")
-const respuestaUnoTexto = document.getElementById("opcion1")
-const respuestaDosTexto = document.getElementById("opcion2")
-const respuesta = document.querySelector(".respuesta")
+const textoOpcionA = document.getElementById("texto-opcion-a")
+const textoOpcionB = document.getElementById("texto-opcion-b")
+const opcionA = document.getElementById("a")
+const opcionB = document.getElementById("b")
+const contenedorDelJuego = document.querySelector(".contenedor-del-juego")
 const botonEnviar = document.getElementById("boton-enviar")
+const infoFinal = document.getElementById("pregunta")
 
 // En este array defino las 10 preguntas, dos opciones por cada una y la respuesta correcta. Cada pregunta es un objeto
 
 datosDelJuego = [
     {
         pregunta: "Que tienes en tu casa?",
-        opcionUno: "perrin",
-        opcionDos: "gatin",
-        correcta: "perro" 
+        opcionUno: "perro",
+        opcionDos: "gato",
+        correcta: "b" 
+    },
+    {
+        pregunta: "Que tienes en tu carro?",
+        opcionUno: "blanco",
+        opcionDos: "negro",
+        correcta: "a" 
+    },
+    {
+        pregunta: "Que tienes en tu calle?",
+        opcionUno: "gente",
+        opcionDos: "arboles",
+        correcta: "a" 
+    },
+    {
+        pregunta: "Que tienes en tu bolsillo?",
+        opcionUno: "monedas",
+        opcionDos: "billetes",
+        correcta: "a" 
+    },
+    {
+        pregunta: "Que tienes en tu mente?",
+        opcionUno: "problemas",
+        opcionDos: "ideas",
+        correcta: "a" 
     }
 ]
 
@@ -26,38 +53,71 @@ let puntaje = 0
 let racha = 0
 
 // 
+
+iniciarJuego()
+
+function iniciarJuego () {
+    inicio.addEventListener("click", (e) => {
+        contenedorDelJuego.style.display = "block"
+        inicio.style.display = "none"
+    })
+}
+
 cargarPregunta()
 
 function cargarPregunta () {
     const datosPreguntaActual = datosDelJuego[preguntaActual]
 
     espacioParaPregunta.innerText = datosPreguntaActual.pregunta
-    respuestaUnoTexto.innerText = datosPreguntaActual.opcionUno
-    respuestaDosTexto.innerText = datosPreguntaActual.opcionDos
-
+    textoOpcionA.innerText = datosPreguntaActual.opcionUno
+    textoOpcionB.innerText = datosPreguntaActual.opcionDos
+    opcionA.checked = false
+    opcionB.checked = false
 }
 
 // Así recibo el evento click en el botón "enviar"
 botonEnviar.addEventListener("click", (e) => enviarRespuesta())
 
 // Esta función toma el valor que tenga el radio button que este checked. Este valor se asigna a la variable respuestaElegida. 
+
 function enviarRespuesta() {
+
+    preguntaActual++
     
     let respuestaElegida
     
-    if (respuestaUnoTexto.checked) {
-        respuestaElegida = respuestaUnoTexto.value
-    } else if (respuestaDosTexto.checked) {
-        respuestaElegida = respuestaDosTexto.value
-    }
-    console.log(respuestaElegida)
-    esCorrecta()
-} 
+    // Aquí compruebo cuál es el respuesta que el usuario eligió
 
-function esCorrecta (respuestaElegida) {
-    if(respuestaElegida === datosDelJuego[preguntaActual].correcta) {
-        console.log("es correcta")
-    } else {
-        console.log("esta mal")
+    if (opcionA.checked) {
+        respuestaElegida = opcionA.value
+    } else if (opcionB.checked) {
+        respuestaElegida = opcionB.value
     }
-}
+    console.log(respuestaElegida) 
+
+    // Aquí comparo la respuesta que el usuario eligió con el valor de respuesta correcta del array de datos.
+    const datosPreguntaActual = datosDelJuego[preguntaActual - 1]
+
+    // Asigno al puntaje de la 'ronda', el index de la pregunta actual del array
+    let puntajePregunta = preguntaActual
+
+    if (respuestaElegida !== datosPreguntaActual.correcta) {
+        racha = 0
+        console.log("es incorrecta", "En la pregunta actual " + preguntaActual, "el puntaje es de " + puntaje, "y la racha es " + racha)
+    }   else {
+        racha++
+        puntaje = puntaje + (puntajePregunta * racha)
+        console.log("es correcta", "En la pregunta actual " + preguntaActual, "el puntaje de esta pregunta es " + puntajePregunta, "el puntaje es de " + puntaje, "y la racha es " + racha)
+    }
+    // Vuelvo a llamar a esta función para cargar la siguiente pregunta. Si no hay más preguntas muestro un mensaje con el puntaje final, el nivel que alcanzó el jugador y la opción de empezar de nuevo. 
+    if (preguntaActual < datosDelJuego.length) {
+        cargarPregunta()
+    } else {
+        infoFinal.innerHTML = `
+        <p class="puntaje-final">puntaje final: ${puntaje}</p>
+        <span class="nivel">Tu nivel es Roberto Baggio!</span>
+        <button class="iniciar" display: inline;" onclick="location.reload()">Intentarlo de nuevo!</button>
+        `
+        botonEnviar.style.display = "none"
+    }
+} 
