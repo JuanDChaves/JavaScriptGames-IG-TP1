610c3cf96a2131dc85e8f2cb4c4721316eba1990
Juan D Chaves <juandavidchaves@protonmail.com>
2021-09-29 23:40:28 -0300

    game 3. Some functionality added

---
+++ juegos/juego3/juego3.js
@@ -0,0 +1,7 @@
+
+let numero1 = parseInt(Math.random(Math.floor) * 100)
+let numero2 = parseInt(Math.random(Math.floor) * 100)
+let respuestaCorrecta = numero1 + numero2
+let operador = ["+", "-"]
+
+console.log(numero1, numero2, respuestaCorrecta)
